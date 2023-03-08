61517c2627f6b6c35242e16621afdbdf3f4556f2
AliceShi25 <shialice2006@gmail.com>
2023-03-08 13:12:20 -0500

    learning

---
+++ vue-project/src/App.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  data() {
+    return {
+      message: "mewmew",
+      sameww: "meow",
+      count: 0,
+    };
+  },
+  methods: {
+    littlemew() {
+      // update component state
+      this.count++;
+    },
+  },
+};
+</script>
+
+<template>
+  <h1 v-bind:class="sameww">{{ message }}</h1>
+  <button @click="littlemew">{{ count }}</button>
+</template>
+
+<style>
+.meow {
+  color: aqua;
+}
+</style>
