7d6f20ab7e5c21bc8c395cfd64f27896ae4e708b
AliceShi25 <shialice2006@gmail.com>
2023-03-08 12:56:35 -0500

    g

---
+++ vue-project/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  data() {
+    return {
+      message: "omg",
+      speaker: "Tler B",
+    };
+  },
+};
+</script>
+
+<template>
+  <div>
+    <h1>{{ message }} {{ speaker }}</h1>
+  </div>
+</template>
