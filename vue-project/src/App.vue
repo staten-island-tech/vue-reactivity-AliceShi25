e7981426e71ff9c25c0d840f1642928501b29f38
AliceShi25 <shialice2006@gmail.com>
2023-03-10 13:13:41 -0500

    Update App.vue

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,4 +1,6 @@
 <script>
+import { Ref } from "vue";
+let id = 0;
 export default {
   data() {
     return {
@@ -7,6 +9,12 @@ export default {
       count: 0,
       text: "",
       lyuvee: true,
+      newTodo: "",
+      todos: [
+        { id: id++, text: "Learn HTML" },
+        { id: id++, text: "Learn JavaScript" },
+        { id: id++, text: "Learn Vue" },
+      ],
     };
   },
   methods: {
@@ -19,6 +27,13 @@ export default {
     },
     toggle() {
       this.lyuvee = !this.lyuvee;
+    },
+    addTodo() {
+      this.todos.push({ id: id++, text: this.newTodo });
+      this.newTodo = "";
+    },
+    removeTodo(todo) {
+      this.todos = this.todos.filter((t) => t !== todo);
     },
   },
 };
@@ -33,6 +48,16 @@ export default {
   </button>
   <input :value="text" @input="onInput" placeholder="Type here" />
   <p>{{ text }}</p>
+  <form @submit.prevent="addTodo">
+    <input v-model="newTodo" />
+    <button>Add Todo</button>
+  </form>
+  <ul>
+    <li v-for="todo in todos" :key="todo.id">
+      {{ todo.text }}
+      <button @click="removeTodo(todo)">X</button>
+    </li>
+  </ul>
 </template>
 
 <style scoped>
