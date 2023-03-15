c39f7bc468973a5f4a98ddf71bd3db9219f982cb
AliceShi25 <shialice2006@gmail.com>
2023-03-15 13:16:58 -0400

    send hep

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,10 +1,15 @@
 <template>
   <div class="hewwo">
     <h1>hello</h1>
-    <h2>{{ weewo }}</h2>
+    <h2 v-if="notcat">{{ weewo }}</h2>
+    <h2 v-else>Yai9dosskjhksa</h2>
     <ul>
       <li v-for="cat in cats" :key="cat">{{ cat }}</li>
     </ul>
+    <div class="button">
+      <button v-on:click="yessir" v-if="mewozzy">Mewozaaa!</button>
+      <button v-on:click="yessir" v-else>wroof</button>
+    </div>
   </div>
 </template>
 
@@ -14,7 +19,18 @@ export default {
     return {
       weewo: "mewo",
       cats: ["cat1", "mewmewo", "bbg", "kitty", "somewo"],
+      notcat: true,
+      mewozzy: true,
     };
+  },
+  methods: {
+    yessir: function () {
+      if (this.mewozzy === false) {
+        this.mewozzy = true;
+      } else {
+        this.mewozzy = false;
+      }
+    },
   },
 };
 </script>
