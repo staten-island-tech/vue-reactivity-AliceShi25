c63702e6cd7466a8d2b579f9f6526685c6adac21
AliceShi25 <shialice2006@gmail.com>
2023-03-16 13:14:05 -0400

    h

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -15,6 +15,7 @@
 
 <script>
 export default {
+  components: {},
   data() {
     return {
       weewo: "mewo",
---
+++ vue-project/src/views/TrialArray.vue
@@ -0,0 +1,15 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  test: [
+    { name: "Tyler", lastName: "Bo", osis: "2048n6125" },
+    { name: "A", lastName: "S", osis: "nnnnnnnnn" },
+    { name: "AB", lastName: "ST", osis: "nnnnwnnnn" },
+  ],
+};
+</script>
+
+<style></style>
