6101d2c1852e49cedf620402d0bb80e02697db5b
AliceShi25 <shialice2006@gmail.com>
2023-03-17 13:33:57 -0400

    hnhh

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -2,7 +2,9 @@
   <div class="hewwo">
     <h1>hello</h1>
     <div class="items">
-      <h2 v-for="star in stars" :key="star.name">{{ star.name }}</h2>
+      <h2 v-for="star in stars" :key="star.name">
+        {{ star.lastName }} {{ star.name }}
+      </h2>
     </div>
   </div>
 </template>
@@ -13,35 +15,34 @@ export default {
   data() {
     return {
       stars: [
-        { name: "Do Ha", lastName: "Bak", group: "K", height: "180", pic: "" },
-        { name: "Woong Ki", lastName: "Cha", group: "K", height: "", pic: "" },
-        { name: "Woo Jin", lastName: "Choi", group: "K", height: "", pic: "" },
-        { name: "Yu Jin", lastName: "Han", group: "K", height: "", pic: "" },
-        { name: "I Chan", lastName: "Jeong", group: "K", height: "", pic: "" },
-        { name: "Yun Seo", lastName: "Ji", group: "K", height: "", pic: "" },
-        { name: "Min Gyu", lastName: "Jung", group: "K", height: "", pic: "" },
-        { name: "Gyu Vin", lastName: "Kim", group: "K", height: "", pic: "" },
-        { name: "Ji Woong", lastName: "Kim", group: "K", height: "", pic: "" },
-        { name: "Tae Rae", lastName: "Kim", group: "K", height: "", pic: "" },
-        { name: "Jun Hyeon", lastName: "Kum", group: "K", height: "", pic: "" },
-        { name: "Da Eul", lastName: "Lee", group: "K", height: "", pic: "" },
-        { name: "Dong Gun", lastName: "Lee", group: "K", height: "", pic: "" },
-        { name: "Dong Yeol", lastName: "Lee", group: "K", height: "", pic: "" },
-        { name: "Hoe Taek", lastName: "Lee", group: "K", height: "", pic: "" },
-        { name: "Ye Dam", lastName: "Lee", group: "K", height: "", pic: "" },
-        { name: "Jun Seo", lastName: "Lim", group: "K", height: "", pic: "" },
-        { name: "Jung Hyun", lastName: "Mun", group: "K", height: "", pic: "" },
-        { name: "Sung Min", lastName: "Oh", group: "K", height: "", pic: "" },
-        { name: "Gun Wook", lastName: "Park", group: "K", height: "", pic: "" },
-        { name: "Han Bin", lastName: "Park", group: "K", height: "", pic: "" },
+        { name: "Do Ha", lastName: "Bak", group: "K", pic: "" },
+        { name: "Woong Ki", lastName: "Cha", group: "K", pic: "" },
+        { name: "Woo Jin", lastName: "Choi", group: "K", pic: "" },
+        { name: "Yu Jin", lastName: "Han", group: "K", pic: "" },
+        { name: "I Chan", lastName: "Jeong", group: "K", pic: "" },
+        { name: "Yun Seo", lastName: "Ji", group: "K", pic: "" },
+        { name: "Min Gyu", lastName: "Jung", group: "K", pic: "" },
+        { name: "Gyu Vin", lastName: "Kim", group: "K", pic: "" },
+        { name: "Ji Woong", lastName: "Kim", group: "K", pic: "" },
+        { name: "Tae Rae", lastName: "Kim", group: "K", pic: "" },
+        { name: "Jun Hyeon", lastName: "Kum", group: "K", pic: "" },
+        { name: "Da Eul", lastName: "Lee", group: "K", pic: "" },
+        { name: "Dong Gun", lastName: "Lee", group: "K", pic: "" },
+        { name: "Dong Yeol", lastName: "Lee", group: "K", pic: "" },
+        { name: "Hoe Taek", lastName: "Lee", group: "K", pic: "" },
+        { name: "Ye Dam", lastName: "Lee", group: "K", pic: "" },
+        { name: "Jun Seo", lastName: "Lim", group: "K", pic: "" },
+        { name: "Jung Hyun", lastName: "Mun", group: "K", pic: "" },
+        { name: "Sung Min", lastName: "Oh", group: "K", pic: "" },
+        { name: "Gun Wook", lastName: "Park", group: "K", pic: "" },
+        { name: "Han Bin", lastName: "Park", group: "K", pic: "" },
         {
           name: "Hyun Been",
           lastName: "Park",
           group: "K",
-          height: "",
           pic: "",
         },
-        { name: "Ji Hoo", lastName: "Park", group: "K", height: "", pic: "" },
+        { name: "Ji Hoo", lastName: "Park", group: "K", pic: "" },
       ],
     };
   },
@@ -62,7 +63,7 @@ export default {
   flex: 0 0 23.5%;
   width: 32vw;
   margin: 25px auto;
-  background-color: red;
+  background-color: pink;
   align-content: center;
   border: solid white;
   border-radius: 25px;
